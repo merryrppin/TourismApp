ce7d3fdfd1a86313736efb371a4b492b09edd128
Alexis Cano <jalexiscc@hotmail.com>
2021-07-12 22:04:22 -0500

    se crean las estructuras del proyecto y las paginas que va a contener

--- IonTourismApp/src/app/app-routing.module.ts
+++ IonTourismApp/src/app/app-routing.module.ts
@@ -3,14 +3,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'maps',
+    loadChildren: () => import('./modules/maps/maps.module').then( m => m.MapsPageModule)
+  },
+
+
+
 ];
 
 @NgModule({
---
+++ IonTourismApp/src/app/layout/main-layout/main-layout.page.ts
@@ -0,0 +1,24 @@
+import { Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-main-layout',
+  templateUrl: './main-layout.page.html',
+  styleUrls: ['./main-layout.page.scss'],
+})
+export class MainLayoutPage implements OnInit {
+  appPages = [
+    { title: ' Girardota', url: '/_mainLayout/maps', icon: '/assets/images/marcacion.svg' },
+  ]
+
+  @ViewChild("content") content: ElementRef
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit() {
+  }
+  openMenu() {
+    this.renderer.setAttribute(this.content.nativeElement, "style","max-width:80%");
+  }
+  closeMenu() {
+    this.renderer.removeAttribute(this.content.nativeElement, "style");
+  }
+}
