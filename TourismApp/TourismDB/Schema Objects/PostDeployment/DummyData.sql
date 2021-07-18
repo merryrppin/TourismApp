3732a6c8ef8e4026b09fc8b03f076c99a17bb2fd
Wilmar González <merryrppin@hotmail.com>
2021-07-17 20:34:40 -0500

    Procedimiento para obtener los sitios turísticos

--- TourismApp/TourismDB/Schema Objects/PostDeployment/DummyData.sql
+++ TourismApp/TourismDB/Schema Objects/PostDeployment/DummyData.sql
@@ -5,3 +5,13 @@ INSERT INTO [dbo].[tblMunicipio]([IdMunicipio], [NombreMunicipio], [Activo]) VAL
 (1, 'Girardota', 1);
 
 SET IDENTITY_INSERT [dbo].[tblMunicipio] OFF
+
+--[dbo].[tblSitioTuristico]
+SET IDENTITY_INSERT [dbo].[tblSitioTuristico] ON
+
+INSERT INTO [dbo].[tblSitioTuristico]([IdSitioTuristico], [NombreSitioTuristicoESP], [IdMunicipio], [Latitud], [Longitud], [Activo]) VALUES
+(1, 'Parque Principal Girardota', 1, 6.3747376, -75.4499254, 1),
+(2, 'Placa Deportiva Barrio el Paraíso', 1, 6.3742815, -75.4463368, 1),
+(3, 'Comfama Girardota', 1, 6.3774803, -75.4505639, 1);
+
+SET IDENTITY_INSERT [dbo].[tblSitioTuristico] OFF
