6c6b750971e9e00b2e6cb5595882b6bc8807f9ef
Wilmar González <merryrppin@hotmail.com>
2021-07-18 15:40:50 -0500

    Puntos dummy de sitios turisticos

---
+++ IonTourismApp/src/app/data/models/sitioturistico.ts
@@ -0,0 +1,11 @@
+export class SitioTuristico{
+    IdSitioTuristico :number;
+	NombreSitioTuristicoESP :string;
+	NombreSitioTuristicoENG :string;
+	IdMunicipio :number;
+	Latitud :number;
+	Longitud :number;
+	Altitud :number;
+	IconoMarcador :string;
+	Activo :boolean;
+}
--- IonTourismApp/src/app/modules/maps/maps.page.ts
+++ IonTourismApp/src/app/modules/maps/maps.page.ts
@@ -6,10 +6,12 @@ import {
   GoogleMapsEvent,
   Marker,
   GoogleMapsAnimation,
-  MyLocation
+  MyLocation,
+  LatLng
 } from "@ionic-native/google-maps";
 
 import { Platform, LoadingController, ToastController } from "@ionic/angular";
+import { SitioTuristico } from "src/app/data/models/sitioturistico";
 
 @Component({
   selector: 'app-maps',
@@ -18,6 +20,8 @@ import { Platform, LoadingController, ToastController } from "@ionic/angular";
 })
 export class MapsPage {
 
+  idioma: string = "ESP"; //TEST 
+  idMunicipio: number = 1; //Girardota
   map: GoogleMap;
   loading: any;
 
@@ -25,7 +29,7 @@ export class MapsPage {
     public loadingCtrl: LoadingController,
     public toastCtrl: ToastController,
     private platform: Platform
-  ) {}
+  ) { }
 
   async ngOnInit() {
     // Debido ngOnInit() inicia antes del evento
@@ -46,10 +50,10 @@ export class MapsPage {
     this.map = GoogleMaps.create("map_canvas", {
       camera: {
         target: {
-          lat: -2.1537488,
-          lng: -79.8883037
+          lat: 6.378543,
+          lng: -75.4464299
         },
-        zoom: 18,
+        zoom: 15,
         tilt: 30
       }
     });
@@ -69,45 +73,49 @@ export class MapsPage {
     // Presentamos el componente creado en el paso anterior
     await this.loading.present();
 
+    
+
+    this.cargarSitiosTuristicos(this.map);
+    this.loading.dismiss();
     // Ejecutamos el método getMyLocation de nuestra propiedad de clase
     // map
     // para obtener nuestra ubicación actual
-    this.map
-      .getMyLocation()
-      .then((location: MyLocation) => {
-        // Una vez obtenida la ubicación cerramos el mensaje de diálogo
-        this.loading.dismiss();
+    // this.map
+    //   .getMyLocation()
+    //   .then((location: MyLocation) => {
+    //     // Una vez obtenida la ubicación cerramos el mensaje de diálogo
+    //     this.loading.dismiss();
 
-        // Movemos la camara a nuestra ubicación con una pequeña animación
-        this.map.animateCamera({
-          target: location.latLng,
-          zoom: 17,
-          tilt: 30
-        });
+    //     // Movemos la camara a nuestra ubicación con una pequeña animación
+    //     this.map.animateCamera({
+    //       target: location.latLng,
+    //       zoom: 17,
+    //       tilt: 30
+    //     });
 
-        // Agregamos un nuevo marcador
-        let marker: Marker = this.map.addMarkerSync({
-          title: "Estoy aquí!",
-          snippet: "This plugin is awesome!",
-          position: location.latLng,
-          animation: GoogleMapsAnimation.BOUNCE
-        });
+    //     // Agregamos un nuevo marcador
+    //     let marker: Marker = this.map.addMarkerSync({
+    //       title: "Estoy aquí!",
+    //       snippet: "This plugin is awesome!",
+    //       position: location.latLng,
+    //       animation: GoogleMapsAnimation.BOUNCE
+    //     });
 
-        // Mostramos un InfoWindow
-        marker.showInfoWindow();
+    //     // Mostramos un InfoWindow
+    //     marker.showInfoWindow();
 
-        // Podemos configurar un evento que se ejecute cuando
-        // se haya dado clic
-        marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
-          this.showToast("clicked!");
-        });
-      })
-      .catch(error => {
-        // En caso de que haya un problema en obtener la
-        // ubicación
-        this.loading.dismiss();
-        this.showToast(error.error_message);
-      });
+    //     // Podemos configurar un evento que se ejecute cuando
+    //     // se haya dado clic
+    //     marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+    //       this.showToast("clicked!");
+    //     });
+    //   })
+    //   .catch(error => {
+    //     // En caso de que haya un problema en obtener la
+    //     // ubicación
+    //     this.loading.dismiss();
+    //     this.showToast(error.error_message);
+    //   });
   }
 
   // Función que muestra un Toast en la parte inferior
@@ -122,4 +130,36 @@ export class MapsPage {
     toast.present();
   }
 
+  cargarSitiosTuristicos(map:GoogleMap) {
+    let jsonRows :string = '[["1","Parque Principal Girardota","1","6.374737600","-75.449925400"],["2","Placa Deportiva Barrio el Paraíso","1","6.374281500","-75.446336800"],["3","Comfama Girardota","1","6.377480300","-75.450563900"]]';
+    let jsonColumns :string = '["IdSitioTuristico","NombreSitioTuristicoESP","IdMunicipio","Latitud","Longitud"]';    
+    let aSitiosTuristicos:SitioTuristico[] = this.arrayMap(jsonRows, jsonColumns);
+    this.dibujarSitiosTuristicos(map, aSitiosTuristicos);
+  }
+
+  arrayMap(jsonRows: string, jsonColumns: string):any[] {
+    let aColumns: string[] = JSON.parse(jsonColumns);
+    let aRows: string[] = JSON.parse(jsonRows);
+    let aData: object[] = [];
+    aRows.forEach(function (aRows) {
+      let objData = {};
+      aColumns.forEach(function (objColumn, indexColumn) {
+        objData[objColumn] = aRows[indexColumn];
+      });
+      aData.push(objData);
+    });
+    return aData;
+  }
+
+  dibujarSitiosTuristicos(map:GoogleMap, SitiosTuristicos: SitioTuristico[]) {
+    SitiosTuristicos.forEach(function (objSitioTuristico) {
+      let latLng: LatLng = new LatLng(objSitioTuristico.Latitud, objSitioTuristico.Longitud);
+
+      let marker: Marker = map.addMarkerSync({
+        title: objSitioTuristico.NombreSitioTuristicoESP,
+        position: latLng,
+        animation: GoogleMapsAnimation.BOUNCE
+      });
+    });
+  }
 }
