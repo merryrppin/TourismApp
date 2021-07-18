d13ef4dd75cb3466259c65ab4c0923a437974245
Wilmar González <merryrppin@hotmail.com>
2021-07-17 20:09:29 -0500

    Consumo del GoogleDirections a traves del api

---
+++ TourismApp/TourismApp/Services/Entities/GoogleDirectionsEntities/OriginDirection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TourismApp.Services.Entities.GoogleDirectionsEntities
+{
+    public class OriginDirection
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+    }
+}
