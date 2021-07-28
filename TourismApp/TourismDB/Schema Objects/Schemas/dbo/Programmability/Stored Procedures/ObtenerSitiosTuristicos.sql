0fb41e9f128e1ff3bbea29dc3a72d20884fcf951
Wilmar González <merryrppin@hotmail.com>
2021-07-27 19:38:05 -0500

    Columna del titulo en la info del sitio turistico

--- TourismApp/TourismDB/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ObtenerSitiosTuristicos.sql
+++ TourismApp/TourismDB/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ObtenerSitiosTuristicos.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[ObtenerSitiosTuristicos]
 	@IdMunicipio INT
 AS BEGIN
-	SELECT st.IdSitioTuristico, st.NombreSitioTuristicoESP, st.IdMunicipio, st.Latitud, st.Longitud, ist.DescripcionESP, ist.DescripcionENG, ist.Imagen
+	SELECT st.IdSitioTuristico, ist.Titulo, st.NombreSitioTuristicoESP, st.IdMunicipio, st.Latitud, st.Longitud, ist.DescripcionESP, ist.DescripcionENG, ist.Imagen, ist.Orden
 	FROM tblSitioTuristico AS st
 	LEFT JOIN tblInfoSitioTuristico AS ist ON st.IdSitioTuristico = ist.IdSitioTuristico
 	WHERE IdMunicipio = @IdMunicipio
--- TourismApp/TourismDB/Schema Objects/Schemas/dbo/Tables/tblInfoSitioTuristico.sql
+++ TourismApp/TourismDB/Schema Objects/Schemas/dbo/Tables/tblInfoSitioTuristico.sql
@@ -2,8 +2,10 @@
 (
 	[IdInfoSitioTuristico] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	[IdSitioTuristico] INT NOT NULL,
+	[Titulo] VARCHAR(250) NULL,
 	[DescripcionESP] VARCHAR(MAX) NULL,
 	[DescripcionENG] VARCHAR(MAX) NULL, 
 	[Imagen] VARCHAR(MAX) NULL,
+    [Orden] INT NOT NULL DEFAULT 0, 
     CONSTRAINT [FK_tblInfoSitioTuristico_tblSitioTuristico] FOREIGN KEY ([IdSitioTuristico]) REFERENCES [tblSitioTuristico]([IdSitioTuristico])
 )
