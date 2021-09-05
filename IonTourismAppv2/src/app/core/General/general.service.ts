0ee037019a37227b31cadc8a6f9261fb2031dbb2
Wilmar González <merryrppin@hotmail.com>
2021-09-05 01:40:39 -0500

    test map google

--- IonTourismAppv2/src/app/core/General/general.service.ts
+++ IonTourismAppv2/src/app/core/General/general.service.ts
@@ -1,12 +1,233 @@
 import { Injectable } from '@angular/core';
+import { AlertController, LoadingController, ToastController } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
+import { ToastOptions } from "@ionic/core";
 
+
+export interface IOptionLoading {
+  message: string;
+  keyboardClose: boolean;
+}
+
+interface IOptionToast {
+  position?: "top" | "bottom" | "middle";
+  duration?: number;
+  color?: "primary" | "success" | "warning" | "danger" | "dark" ;
+  cssClass?:"toastStyle";
+}
+interface IOptionAlertConfirm {
+  message: string;
+  buttonTextYes?: string;
+  buttonTextNo?: string;
+  enableCountDown?: boolean;
+  /** Set time on seconds */
+  timeCountDown?: number;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class GeneralService {
   public currentLanguage: string = "ESP";//TODO: Valor por defecto, al cambiar se debe mantener en el storage de la aplicación y cargar desde ahí
+  private _toast: HTMLIonToastElement;
+  readonly limitToast: number = 3000;
+  private messageSource = new BehaviorSubject("Marcacion SEF");
+  currentMessage = this.messageSource.asObservable();
+  constructor(private loadingController: LoadingController, private toastCtrl: ToastController,private alertController: AlertController) { }
 
-  constructor() { }
+  toastDissmiss() {
+    setTimeout(() => {
+      if (this._toast) this._toast.dismiss();
+    });
+  }
+
+  async presentLoading(opts: IOptionLoading): Promise<HTMLIonLoadingElement> {
+    const loading = await this.loadingController.create({
+      message: opts.message,
+      backdropDismiss: false,
+      spinner: "crescent",
+      mode: "ios",
+      keyboardClose: opts.keyboardClose
+    });
+
+    await loading.present();
+    return loading;
+  }
+
+  async showMessage(
+    message: string,
+    showButtonDismiss: boolean = false,
+    opt: IOptionToast = { position: "top", color: "dark"}
+  ): Promise<HTMLIonToastElement> {
+    opt.color = "dark";
+    return this.toastMessage(message, showButtonDismiss, opt);
+  }
+
+  async showErrorMessage(
+    message: string,
+    showButtonDismiss: boolean = false,
+    opt: IOptionToast = { position: "top", color: "danger" }
+  ): Promise<HTMLIonToastElement> {
+    opt.color = "danger";
+    return this.toastMessage(message, showButtonDismiss, opt);
+  }
+
+  async showInfoMessage(
+    message: string,
+    showButtonDismiss: boolean = false,
+    opt: IOptionToast = { position: "top", color: "primary" }
+  ): Promise<HTMLIonToastElement> {
+    opt.color = "primary";
+    return this.toastMessage(message, showButtonDismiss, opt);
+  }
+
+  async showWarningMessage(
+    message: string,
+    showButtonDismiss: boolean = false,
+    opt: IOptionToast = { position: "top", color: "warning" }
+  ): Promise<HTMLIonToastElement> {
+    opt.color = "warning";
+    return this.toastMessage(message, showButtonDismiss, opt);
+  }
+
+  async showSuccessMessage(
+    message: string,
+    showButtonDismiss: boolean = false,
+    opt: IOptionToast = { position: "top", color: "success" }
+  ): Promise<HTMLIonToastElement> {
+    opt.color = "success";
+    return this.toastMessage(message, showButtonDismiss, opt);
+  }
+
+  private async toastMessage(
+    message: string,
+    showButtonDismiss: boolean = false,
+    opt: IOptionToast
+  ): Promise<HTMLIonToastElement> {
+    let options: ToastOptions = {
+      message: message,
+      duration: opt.duration > 0 ? opt.duration : 7000,
+      position: opt.position ? opt.position : "top",
+      color: opt.color ? opt.color : "dark"
+    };
+    this._toast = await this.toastCtrl.create(options);
+    this._toast.present();
+    return this._toast;
+  }
+
+  showToastSuccess(message:string,duration:number) {
+    this.toastCtrl.create({
+      message: message,
+      position: 'bottom',
+      duration:duration,
+      cssClass: 'my-custom-class',
+      buttons: [
+        {
+          side: 'start',
+          icon: '../../assets/images/success.svg',
+        }, {
+          side: 'end',
+          text: 'X',
+          role: 'cancel',
+        }
+      ]
+    }).then((obj) => {
+      obj.present();
+    });
+  }
+
+  showToastError(message:string,duration:number) {
+    this.toastCtrl.create({
+      message: message,
+      position: 'bottom',
+      duration:duration,
+      cssClass: 'my-custom-class',
+      buttons: [
+        {
+          side: 'start',
+          icon: '../../assets/images/information.svg',
+        }, {
+          side: 'end',
+          text: 'X',
+          role: 'cancel',
+        }
+      ]
+    }).then((obj) => {
+      obj.present();
+    });
+  }
+
+  showToastInfo(message:string,duration:number) {
+    this.toastCtrl.create({
+      message: message,
+      position: 'bottom',
+      duration:duration,
+      cssClass: 'my-custom-class',
+      buttons: [
+        {
+          side: 'start',
+          icon: '../../assets/images/information.svg',
+        }, {
+          side: 'end',
+          icon: 'close',
+          role: 'cancel',
+        }
+      ]
+    }).then((obj) => {
+      obj.present();
+    });
+  }
+
+  async changeMessage(Title: string) {
+    this.messageSource.next(Title)
+  }
+
+  async showConfirmedMessage(opt: IOptionAlertConfirm) {
+    let labelYes = opt.buttonTextYes ? opt.buttonTextYes : "Yes";
+    const labelNo = opt.buttonTextYes ? opt.buttonTextNo : "No";
+    const cssClassDisableButton = "confirm-disable";
+    let timeCount = opt.timeCountDown ? opt.timeCountDown : 5;
+    return new Promise<boolean>(async (resolve, reject) => {
+      let confirm = await this.alertController.create({
+        message: opt.message,
+        mode: "ios",
+        buttons: [
+          {
+            text: opt.enableCountDown ? `${labelYes} (${timeCount})` : labelYes,
+            handler: () => {
+              resolve(true);
+            },
+            cssClass: opt.enableCountDown ? cssClassDisableButton : ""
+          },
+          {
+            text: labelNo,
+            handler: () => {
+              resolve(false);
+            },
+            role: "cancel"
+          }
+        ]
+      });
+      await confirm.present().then(() => {
+        if (opt.enableCountDown) {
+          const btnYes = confirm.querySelector(
+            "div.alert-button-group button:nth-of-type(1)"
+          );
+          const spanText = <HTMLSpanElement>btnYes.querySelector("span");
+          const timeInterval = 1000;
+          const interval = setInterval(() => {
+            timeCount--;
+            if (timeCount <= 0) {
+              spanText.innerText = `${labelYes}`;
+              btnYes.classList.remove(cssClassDisableButton);
+              clearInterval(interval);
+            } else {
+              spanText.innerText = `${labelYes} (${timeCount})`;
+            }
+          }, timeInterval);
+        }
+      });
+    });
+  }
 
   getCurrentLanguage(): string {
     return this.currentLanguage;
--- IonTourismAppv2/src/app/modules/maps/maps.page.ts
+++ IonTourismAppv2/src/app/modules/maps/maps.page.ts
@@ -7,12 +7,31 @@ import { SitioTuristico } from 'src/app/data/models/sitioturistico';
 import { GoogleMapComponent } from 'src/app/shared/customcontrols/google-maps/google-map.component';
 import { GeneralService } from '../../core/General/general.service';
 
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  Marker,
+  GoogleMapsAnimation,
+  MyLocation,
+  LatLng,
+  MarkerOptions,
+  Polyline,
+  PolylineOptions
+} from "@ionic-native/google-maps";
+import { SyncService } from 'src/app/core/sync/sync.service';
+import { Geolocation as Geoc, Geoposition } from '@ionic-native/geolocation/ngx';
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.page.html',
   styleUrls: ['./maps.page.scss'],
 })
 export class MapsPage {
+
+  directionsService = new google.maps.DirectionsService;
+  directionsDisplay = new google.maps.DirectionsRenderer;
+
+
   @ViewChild(GoogleMapComponent) mapComponent: GoogleMapComponent;
   idioma: string = "ESP"; //TEST 
   idMunicipio: number = 1; //Girardota
@@ -24,9 +43,8 @@ export class MapsPage {
   initialMapLoad: boolean = false;
   aSitiosTuristicosUnique: SitioTuristico[];
 
-  currentMarkerPosition: any = null;
-  //markerUser: Marker;
-  //currentMarkerPosition: LatLng = null;
+  markerUser: Marker;
+  currentMarkerPosition: LatLng = null;
 
   @Input('apiKey') apiKey: string;
   public map: any;
@@ -35,7 +53,11 @@ export class MapsPage {
   private networkHandler = null;
   generalService: GeneralService;
 
-  constructor(private renderer: Renderer2, private element: ElementRef, @Inject(DOCUMENT) private _document) {
+  // @ViewChild('map_canvas') mapView: ElementRef;
+  constructor(
+    //private renderer: Renderer2, private element: ElementRef, @Inject(DOCUMENT) private _document
+    private syncService:SyncService, private geolocation: Geoc
+  ) {
   }
 
   getCurrentLanguageESP(): boolean {
@@ -46,9 +68,220 @@ export class MapsPage {
     return this.generalService.getCurrentLanguage();
   }
 
-  drawRouteFromMarker() {
-
+  ionViewWillEnter(){
+    if (!this.initialMapLoad) {
+      this.map.setDiv('map');
+      this.map.setVisible(true);
+      setTimeout(()=>{
+        // hack to fix occasionally disappearing map
+        this.map.setDiv('map');
+      }, 1000);      
+    } else {
+      this.initialMapLoad = false;
+    }
   }
+
+  ionViewWillLeave() {
+    // unset div & visibility on exit
+    this.map.setVisible(false);
+    this.map.setDiv(null);
+  }
+
+  async cargarDatosMunicipio(){
+    let data = await this.syncService.descargarDatosMunicipio();
+    this.datosMunicipio = this.arrayMap(data.value[0].rows,data.value[0].columns);
+
+    let objData : DataAcordeon = new DataAcordeon();
+    objData.Nombre = this.datosMunicipio[0].NombreMunicipio;
+    objData.NombreENG = this.datosMunicipio[0].NombreMunicipio;
+    objData.ValorESP = this.datosMunicipio[0].ValorESP;
+    objData.ValorENG = this.datosMunicipio[0].ValorENG;
+    objData.Imagen = this.datosMunicipio[0].Imagen;
+    objData.Orden = this.datosMunicipio[0].Orden;
+    this.datosSitioTuristico = [objData]
+  }
+
+  loadMap() {
+    // Esta función inicializa la propiedad de clase
+    // map
+    // que va a contener el control de nuestro mapa de google
+
+    // Para crear nuestro mapa debemos enviar como parametros
+    // el id del div en donde se va a renderizar el mapa (paso anterior)
+    // y las opciones que configuran nuestro mapa
+    this.map = GoogleMaps.create("map_canvas", {
+      camera: {
+        target: {
+          lat: 6.378543,
+          lng: -75.4464299
+        },
+        zoom: 15,
+        tilt: 30
+      }
+    });
+  }
+
+  initiliazeCurrentPosition(){
+      this.geolocation.getCurrentPosition().then((resp) => {
+      let latLng: LatLng = new LatLng(resp.coords.latitude, resp.coords.longitude);
+  
+      let markerOptions: MarkerOptions = {
+        clickable : false,
+        disableAutoPan: true,
+        position: latLng,
+        animation: GoogleMapsAnimation.BOUNCE,
+        icon: "https://webflowers-wmalpha-rf.azurewebsites.net/Images/user.png"
+      };
+
+      this.markerUser = this.map.addMarkerSync(markerOptions);
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+     
+     let watch = this.geolocation.watchPosition();
+     watch.subscribe((data: any) => {
+      let latLng: LatLng = new LatLng(data.coords.latitude, data.coords.longitude);
+      this.markerUser.setPosition(latLng);
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+     });
+  }
+
+  async localizar() {
+    // const loading = await this.generalService.presentLoading({
+    //   message: "por favor espere...",
+    //   keyboardClose: false
+    // });
+    this.currentMarkerPosition = null;
+    // Limpiamos todos los elementos de nuestro mapa
+    this.map.clear();
+    // Presentamos el componente creado en el paso anterior
+    // loading.present();
+    await this.cargarSitiosTuristicos();
+    this.initiliazeCurrentPosition();
+    // loading.dismiss();   
+  }
+
+  async cargarSitiosTuristicos() {
+    let data = await this.syncService.descargarDatos()
+    let jsonRows  = data.value[0].rows;
+    let jsonColumns  = data.value[0].columns;    
+    this.aSitiosTuristicos = this.arrayMap(jsonRows, jsonColumns);
+    this.aSitiosTuristicosUnique = this.aSitiosTuristicos.filter((objSitioTuristico, i, aSitioTuristico) => aSitioTuristico.findIndex(t => t.IdSitioTuristico === objSitioTuristico.IdSitioTuristico) === i);
+
+    await this.dibujarSitiosTuristicos(this);
+  }
+
+  arrayMap(aRows: any[], aColumns: any[]):any[] {
+    let aData: object[] = [];
+    aRows.forEach(function (aRows) {
+      let objData = {};
+      aColumns.forEach(function (objColumn, indexColumn) {
+        objData[objColumn] = aRows[indexColumn];
+      });
+      aData.push(objData);
+    });
+    return aData;
+  }
+
+  dibujarSitiosTuristicos(objThis:any) {
+    this.aSitiosTuristicosUnique.forEach(function (objSitioTuristico) {
+      let latLng: LatLng = new LatLng(objSitioTuristico.Latitud, objSitioTuristico.Longitud);
+  
+      let markerOptions: MarkerOptions = {
+        title: objSitioTuristico.NombreSitioTuristicoESP,
+        position: latLng,
+        animation: GoogleMapsAnimation.BOUNCE,
+        SitioTuristico : objSitioTuristico,
+        icon: objSitioTuristico.IconoMarcador
+      };
+
+      let marker: Marker = objThis.map.addMarkerSync(markerOptions);
+      
+      marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe((params: any) => {
+        objThis.datosSitioTuristico = []
+        let marker: Marker = <Marker>params[1];
+        let SitioTuristico: SitioTuristico = marker.get('SitioTuristico');
+        let aSitiosTuristicos = objThis.aSitiosTuristicos.filter(obj=> obj.IdSitioTuristico === SitioTuristico.IdSitioTuristico);
+        let objData : DataAcordeon = new DataAcordeon();
+        let pr = aSitiosTuristicos.map(o=>({
+          Nombre:o.Titulo,
+          NombreENG:o.TituloENG,
+          ValorESP:o.DescripcionESP,
+          ValorENG:o.DescripcionENG,
+          Imagen:o.Imagen,
+          Orden:o.Orden
+        }))
+     
+        objThis.datosSitioTuristico = pr;
+        objThis.currentMarkerPosition = marker.get('position');
+        objThis.zone.run(() => {});
+      });
+    });
+  }
+
+  calculateAndDisplayRoute(originDirection: string, destinationDirection: string) {
+    let objThis = this;
+    this.directionsService.route({
+      origin: originDirection,
+      destination: destinationDirection,
+      // travelMode: 'DRIVING'
+    }, (response, status) => {
+      if (status === 'OK') {
+        this.drawRoute(response, objThis);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
+  drawRoute(route: any, objThis: any){
+    this.map.clear();
+    this.initiliazeCurrentPosition();
+    this.dibujarSitiosTuristicos(this);
+    let coordinates: LatLng[] = [];
+    let origin: LatLng;
+    let destination: LatLng;
+    let routePoints: any[]= route.routes[0].legs[0].steps;
+    routePoints.forEach(function (objRoutePoint) {
+      let aPath: any[] = objRoutePoint.path;
+      aPath.forEach(function(objPath, index){
+        destination = new LatLng(objPath.lat(), objPath.lng());
+        coordinates.push(new LatLng(destination.lat, destination.lng));
+      });
+
+    });
+    objThis.drawLineMap(coordinates, objThis);
+  }
+
+  drawLineMap(coordinates: LatLng[], objThis: any){
+    let optionsPolyline: PolylineOptions = {
+      points: coordinates,
+      color: '#1e315a'
+    };
+    this.map.addPolyline(optionsPolyline);
+  }
+
+  drawRouteFromMarker(){
+    this.getPosition(this.currentMarkerPosition);
+  }
+
+  getPosition(latLng: LatLng){
+    this.geolocation.getCurrentPosition()
+    .then(data => {
+      let destinationDirection : string = latLng.lat+', '+latLng.lng;
+      let originDirection : string = data.coords.latitude+', '+data.coords.longitude;
+      this.calculateAndDisplayRoute(originDirection, destinationDirection);
+    })
+    .catch(error =>{
+      console.log(error);
+    })
+  }
+
+  // drawRouteFromMarker() {
+
+  // }
 
   // async ionViewDidEnter() {
   //   const boundingRect = this.mapView.nativeElement.getBoundingClientRect() as DOMRect;
