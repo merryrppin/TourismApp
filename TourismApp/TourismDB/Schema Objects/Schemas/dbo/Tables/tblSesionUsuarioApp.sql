3710cd7cf63a1e2747937a47bd4d23f9c598c173
Wilmar González <merryrppin@hotmail.com>
2021-09-12 19:12:57 -0500

    tablas y procedimientos para el manejo de la sesión

---
+++ TourismApp/TourismDB/Schema Objects/Schemas/dbo/Tables/tblSesionUsuarioApp.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblSesionUsuarioApp]
+(
+	[IdSesionUsuarioApp] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[IdUsuario] INT NOT NULL, 
+    [SesionActiva] BIT NOT NULL DEFAULT (1), 
+    [UltimaFechaLogin] DATETIME NOT NULL, 
+    [IdSesion] UNIQUEIDENTIFIER NOT NULL , 
+    CONSTRAINT [FK_tblSesionUsuarioApp_tblSesionUsuario] FOREIGN KEY ([IdUsuario]) REFERENCES [tblUsuarioApp]([IdUsuario])
+)
