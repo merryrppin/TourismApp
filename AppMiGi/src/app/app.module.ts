3e8a1afe1a89ec9ecf246e1bfb885f76ed2b3273
Wilmar González <merryrppin@hotmail.com>
2021-12-04 00:36:30 -0500

    mapa genérico para pruebas e implementaciones

--- AppMiGi/src/app/app.module.ts
+++ AppMiGi/src/app/app.module.ts
@@ -13,11 +13,14 @@ import { GooglePlus } from '@ionic-native/google-plus/ngx';
 import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
 
 import {HttpClientModule } from "@angular/common/http"
+
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [HttpClientModule, BrowserModule, BrowserAnimationsModule, IonicModule.forRoot(), AppRoutingModule, IonicModule],
-  providers: [GooglePlus, Facebook, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },NavParams],
+  providers: [GooglePlus, Facebook, Geolocation, NativeGeocoder, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },NavParams],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
